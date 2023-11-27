5b75e39ac28e31b6c84325c47068258a7209c1af
Mohammad Fathi <mohfabdou@gmail.com>
2023-11-28 01:24:07 +0200

    trying to fix 9th

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = (str.split()[5] + " " + str.split()[6] + " with Python")
+str = (str[39:66] + " with Python")
 print(str)

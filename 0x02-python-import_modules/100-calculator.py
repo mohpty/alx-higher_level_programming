e010b95b1670d62b72213c1e562b22dcb9925a1a
Mohammad Fathi <mohfabdou@gmail.com>
2023-11-30 18:22:19 +0200

    solving first adv

---
+++ 0x02-python-import_modules/100-calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+from sys import argv, exit
+from calculator_1 import add, sub, div, mul
+
+if __name__ == '__main__':
+    argc = len(argv)
+    if argc != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+    result = 0
+    if operator not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        if operator == '+':
+            result = add(a, b)
+        elif operator == '-':
+            result = sub(a, b)
+        elif operator == '*':
+            result = mul(a, b)
+        elif operator == '/':
+            result = div(a, b)
+        print("{} {} {} = {}".format(a, operator, b, result))
+        exit()

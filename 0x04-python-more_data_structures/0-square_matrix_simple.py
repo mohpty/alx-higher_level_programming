7bee19fac2effb1194f36bd37d4739316a174003
Mohammad Fathi <mohfabdou@gmail.com>
2023-12-06 16:30:20 +0200

    first task

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    out = [matrix[i][:] for i in range(len(matrix))]
+    for i in range(len(out)):
+        for j in range(len(out[i])):
+                out[i][j] *= out[i][j]
+    return out

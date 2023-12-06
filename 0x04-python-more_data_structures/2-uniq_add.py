4e7e4ca1d3e3f82006c98e4056aad027481ca41f
Mohammad Fathi <mohfabdou@gmail.com>
2023-12-06 17:04:32 +0200

    3rd task

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    out = []
+    uniq = {i:0 for i in my_list}
+    for i in my_list:
+        if uniq[i] == 0:
+            out.append(i)
+            uniq[i] = 1
+        else:
+            pass
+    return sum(out)

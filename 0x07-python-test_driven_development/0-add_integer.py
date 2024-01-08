8fc234cde9c8fb48e7b5fad74b5888f97f431821
Mohammad Fathi <mohfabdou@gmail.com>
2024-01-08 21:07:27 +0200

    fixed pep 8 styling

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Defines a function that adds two numbers"""
 
+
 def add_integer(a, b=98):
     """
-    Returns a combination of the two numbers a and b (default=98)
-
+    Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
     Raises:
-        TypeError if one of the arguments is not a number (Float or Int)
+        TypeError: If either of a or b is a non-integer and non-float.
     """
-     if ((not isinstance(a, int) and not isinstance(a, float))):
-         raise TypeError("a must be an integer")
-     if ((not isinstance(b, int) and not isinstance(b, float))):
-         raise TypeError("b must be an integer")
-     return (int(a) + int(b))
-
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))

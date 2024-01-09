925bf2f3eaa4631793bf1bcb81cf7c2f49642e1d
Mohammad Fathi <mohfabdou@gmail.com>
2024-01-09 23:24:27 +0200

    trying to solve issue in no 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,5 +6,11 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    with open('add_item.json', 'w', encoding='utf-8') as f:
+    with open('add_item.json', 'r+', encoding='utf-8') as f:
+        obj = json.loads(f.readline())
+        for i in args:
+            if i in obj:
+                pass
+            else:
+                obj.append(i)
         f.write(json.dumps(args))

c14095e35085c0e3f116c27e33ed57a366c3f4f4
Mohammad Fathi <mohfabdou@gmail.com>
2024-01-09 23:36:17 +0200

    changing the order of the statements in 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """Includes a function that store its args in a file"""
-import json
-import sys
 
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+filename = 'add_item.json'
 
 try:
-    arr = load_from_json_file('add_item.json')
+    arr = load_from_json_file(filename)
 except FileNotFoundError:
     arr = []
 
 for i in sys.argv[1:]:
     arr.append(i)
 
-save_to_json_file(arr, 'add_item.json')
+save_to_json_file(arr, filename)

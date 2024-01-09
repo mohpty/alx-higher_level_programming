4c9564cba70e152dfd8efaa2c787d8f77d90bb80
Mohammad Fathi <mohfabdou@gmail.com>
2024-01-09 23:17:52 +0200

    solving 8th task

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Includes a function that store its args in a file"""
+import json
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    with open('add_item.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(args))

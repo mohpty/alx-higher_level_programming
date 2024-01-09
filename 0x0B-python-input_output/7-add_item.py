8520e869a40ad51c5935e3287fe9bd9da3ae7bf3
Mohammad Fathi <mohfabdou@gmail.com>
2024-01-09 23:19:31 +0200

    fixing some issues with 7th

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,5 +6,5 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    with open('add_item.json', 'w', encoding='utf-8') as f:
+    with open('add_item.json', 'a+', encoding='utf-8') as f:
         f.write(json.dumps(args))

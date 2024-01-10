41d45aaa2d86d983c0b3dddfc6e1f6d7cf449e01
Mohammad Fathi <mohfabdou@gmail.com>
2024-01-10 21:46:44 +0200

    trying to fix task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,7 +2,7 @@
 """Contains the function: say_my_name"""
 
 
-def say_my_name(first_name, last_name):
+def say_my_name(first_name, last_name=""):
     """Prints `My name is first_name last_name`"""
     if type(first_name) is not str:
         raise TypeError('first_name must be a string')

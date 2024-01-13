9af0fa62ed1690a56fc1551bc2b76b0a29847c5f
Mohammad Fathi <mohfabdou@gmail.com>
2024-01-13 23:19:10 +0200

    solved task no.11

--- 0x0C-python-almost_a_cicle/models/square.py
+++ 0x0C-python-almost_a_cicle/models/square.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """This module contains the definition of the class square"""
 from . import rectangle as rect
-
+from . import validation as v
 
 class Square(rect.Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size // 2, size // 2, x, y, id)
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        v.isInt('width', value)
+        v.biggerThanZero('width', value)
+        self.width = value
+        self.height = value
 
     def __str__(self):
-        return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y, self.height * self.width)
+        return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y, self.height)

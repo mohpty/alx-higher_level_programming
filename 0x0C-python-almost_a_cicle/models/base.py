51efa71f6877b14892afc28998999a06e93bccb5
Mohammad Fathi <mohfabdou@gmail.com>
2024-01-14 00:01:46 +0200

    solved task no.15

--- 0x0C-python-almost_a_cicle/models/base.py
+++ 0x0C-python-almost_a_cicle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Contains declaration of class Base"""
-
+import json
 
 class Base:
     __nb_objects = 0
@@ -11,3 +11,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)

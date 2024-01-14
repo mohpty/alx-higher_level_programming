41cb674bab2d27fce3bfac4d7838c4daee2a0390
Mohammad Fathi <mohfabdou@gmail.com>
2024-01-14 23:30:33 +0200

    solving task no. 16

--- 0x0C-python-almost_a_cicle/models/base.py
+++ 0x0C-python-almost_a_cicle/models/base.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Contains declaration of class Base"""
 import json
-
+from . import rectangle
 class Base:
     __nb_objects = 0
 
@@ -18,3 +18,17 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes JSON representation of list_objs to a file"""
+        filename = cls.__name__ + '.json'
+        content = []
+        if list_objs is not None:
+            for obj in list_objs:
+                obj = obj.to_dictionary()
+                obj_dict = json.loads(cls.to_json_string(obj))
+                content.append(obj_dict)
+
+        with open(filename, 'w') as f:
+            json.dump(content, f)

546a01fc70944a9aab30c5d62dbe53e8469d1fd2
Mohammad Fathi <mohfabdou@gmail.com>
2024-01-16 13:55:13 +0200

    trying to fix some issues

--- 0x0C-python-almost_a_cicle/models/base.py
+++ 0x0C-python-almost_a_cicle/models/base.py
@@ -9,6 +9,11 @@ class Base:
 
     def __init__(self, id=None):
         if id is not None:
+            if type(id) is not int:
+                raise TypeError('id must be an integer')
+            if id < 0:
+                raise ValueError('id must be >= 0')
+
             self.id = id
         else:
             Base.__nb_objects += 1

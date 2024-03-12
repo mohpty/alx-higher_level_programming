58c85bb19db822a2ce4e2fd57da4fe9fa944068c
Mohammad Fathi <mohfabdou@gmail.com>
2024-03-12 21:39:28 +0300

    solved no.7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let number = Number(process.argv[2]);
+if(isNaN(number))
+{
+	console.log('Missing number of occurrences');
+}
+for (let i = 0; i < number; i++)
+{
+	console.log('C is fun');
+}

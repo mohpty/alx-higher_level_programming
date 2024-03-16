5de3aea41da5a0835f2c09c02950c19af6c16e2f
Mohammad Fathi <mohfabdou@gmail.com>
2024-03-16 04:40:15 +0300

    solving no.1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Performing basic database connection and executing a simple sql script
+that list all states starting with uppercase N
+USAGE: ./0-select_states.py [username] [password] [database]"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1],
+                         password=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    for state in cursor.fetchall():
+        print(state)

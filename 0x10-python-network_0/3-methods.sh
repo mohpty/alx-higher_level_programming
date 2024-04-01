6a9f3ee5382137905bb72ad23065bc2725ebf0cf
Mohammad Fathi <mohfabdou@gmail.com>
2024-04-01 20:30:16 +0300

    fixing 3 and 4

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display all methods that a url accepts
-curl -sI OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
+curl -Is "$1" | grep "Allow:" | cut -d ":" -f 2 | cut -c 2- | rev | cut -c 2- | rev
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends GET request to a url with a variable in the header
-curl -s "$1" -H "X-School-User-Id: 98"
+curl -sH "X-School-User-Id: 98" "${1}"
